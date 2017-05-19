26870119388ac0b35bcdd667a94f69842d0da505
light <kirill.gus@gmail.com>
2017-05-19 22:54:11 +0300

    Using a simpler NgFor

---
+++ src/app/clear-ngfor.directive.spec.ts
@@ -0,0 +1,8 @@
+import { ClearNgforDirective } from './clear-ngfor.directive';
+
+describe('ClearNgforDirective', () => {
+  it('should create an instance', () => {
+    const directive = new ClearNgforDirective();
+    expect(directive).toBeTruthy();
+  });
+});
